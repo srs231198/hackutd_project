484342005246211398d36396a386cf3c53a74016
Shreyas Raj Singh <sxs170100@utdallas.edu>
2021-11-13 21:28:45 -0600

    Added API call skeleton

--- backend/azureDB.py
+++ backend/azureDB.py
@@ -1,11 +1,11 @@
 import psycopg2
 
 # Update connection string information
-host = "<server-name>"
-dbname = "<database-name>"
-user = "<admin-username>"
-password = "<admin-password>"
-sslmode = "require"
+host = "freshoff.postgres.database.azure.com"
+dbname = "postgres"
+user = "freshoff@freshoff"
+password = "y_eUbwd3Pm"
+sslmode = "disable"
 
 # Construct connection string
 conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
@@ -15,18 +15,18 @@ print("Connection established")
 cursor = conn.cursor()
 
 # Drop previous table of same name if one exists
-cursor.execute("DROP TABLE IF EXISTS inventory;")
-print("Finished dropping table (if existed)")
+# cursor.execute("DROP TABLE IF EXISTS inventory;")
+# print("Finished dropping table (if existed)")
 
 # Create a table
-cursor.execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
-print("Finished creating table")
+# cursor.execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
+# print("Finished creating table")
 
 # Insert some data into the table
-cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("banana", 150))
-cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("orange", 154))
-cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("apple", 100))
-print("Inserted 3 rows of data")
+# cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("banana", 150))
+# cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("orange", 154))
+# cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("apple", 100))
+# print("Inserted 3 rows of data")
 
 # Clean up
 conn.commit()
--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,21 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# GET request to return the recommended majors fo the user
+@app.get("/majors/")
+def return_major():
+    return ["CS", "SE", "ACCT"]
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# GET request to return the recommended classes for the user
+@app.get("/classes/")
+def return_classes():
+    return ["CS 2336", "CS 1337", "CS 4301"]
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# POST request to 
+@app.post("/post/")
+def post_func():
+    pass
