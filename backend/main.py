131c9e4cca19d15cd65bf89256dc81cdf52bbb9c
Shreyas Raj Singh <sxs170100@utdallas.edu>
2021-11-13 23:20:57 -0600

    Implemented POST Method schema

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from types import resolve_bases
 from typing import Optional
 
 from fastapi import FastAPI
@@ -5,17 +6,34 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+recommended_majors = ["CS", "SE", "ACCT"]
+recommended_classes = ["CS 2336", "CS 1337", "CS 4301"]
+
+
+# Base model schema for the graduate info
+# which will be provided in the request body of the POST request
+# with the "/graduate/" path
+class graduate_info(BaseModel):
+    industry: str
+    role: str
+    courses: str
+
 # GET request to return the recommended majors fo the user
 @app.get("/majors/")
 def return_major():
-    return ["CS", "SE", "ACCT"]
+    return recommended_majors
 
 # GET request to return the recommended classes for the user
 @app.get("/classes/")
 def return_classes():
-    return ["CS 2336", "CS 1337", "CS 4301"]
+    return recommended_classes
 
 # POST request to 
-@app.post("/post/")
-def post_func():
-    pass
+# {
+#     industry:""
+#     role:""
+#     courses:[]
+# }
+@app.post("/graduate/")
+def student_info(info: graduate_info):
+    return info
