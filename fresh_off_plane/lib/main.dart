459b92168923ec43e3d463946b292204303bad19
Jacob Dong <jjd180002@utdallas.edu>
2021-11-14 00:44:47 -0600

    merging romans and mine things
    yee yee

--- fresh_off_plane/lib/main.dart
+++ fresh_off_plane/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -17,79 +18,193 @@ class FirstRoute extends StatelessWidget {
         title: const Text('Fresh Off The Plane Home Page'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget> [
-            ElevatedButton(
-              child: const Text('Important Links'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SecondRoute()),
-                );
-              },
-            ),
-            ElevatedButton(
-              child: const Text('Insurance'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ThirdRoute()),
-                );
-              },
-            ),
-            ElevatedButton(
-              child: const Text('Help me choose courses'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const CourseHelper()),
-                );
-              },
-            ),
-          ]
-        )
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
+                Text(
+                  'Congrats and welcome to the University of Texas at Dallas!',
+                ),
+                ElevatedButton(
+                  child: const Text('Important Links'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const LinksPage()),
+                    );
+                  },
+                ),
+                ElevatedButton(
+                  child: const Text('Insurance Info'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Insurance()),
+                    );
+                  },
+                ),
+                ElevatedButton(
+                  child: const Text('page4 classes?'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const CourseHelper()),
+                    );
+                  },
+                ),
+              ]
+          )
       ),
     );
   }
 }
 
-class SecondRoute extends StatelessWidget {
-  const SecondRoute({Key? key}) : super(key: key);
+class LinksPage extends StatelessWidget {
+  const LinksPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("page 2, something something important links"),
+        title: const Text("Important links"),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              const Text(
+                'Important Links: ',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                '\neLearning (Grades, Assignments, Announcements)',
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _launchEBrowser();
+                },
+                child: const Text('eLearning'),
+              ),
+              const Text(
+                '\nGalaxy (Academics, Bursar, Financial Aid, Forms, etc)',
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _launchGalaxyBrowser();
+                },
+                child: const Text('Galaxy'),
+              ),
+              const Text(
+                '\nCoursebook (Listing of Classes)',
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _launchCoursebookBrowser();
+                },
+                child: const Text('Coursebook'),
+              ),
+              const Text(
+                '\nOffice of Information Technology (Tech Help)',
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _launchOITBrowser();
+                },
+                child: const Text('OIT'),
+              ),
+              const Text(
+                '\nBursar Office (EZPay, Tuition, Fees)',
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _launchBursarBrowser();
+                },
+                child: const Text('Bursar Office'),
+              ),
+              const Text(
+                '\n\n\n\n\n\n\n\n\n\n\n',
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Go back'),
+              ),
+            ]
         ),
       ),
     );
   }
 }
 
-class ThirdRoute extends StatelessWidget {
-  const ThirdRoute({Key? key}) : super(key: key);
+class Insurance extends StatelessWidget {
+  const Insurance({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("page 3, insurance ? if we are doing something"),
+        title: const Text("Insurance Info"),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              const Text(
+                'UTD International Student Insurance Requirements:',
+                textAlign: TextAlign.center,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _launchInsBrowser();
+                },
+                child: const Text('UT Dallas Insurance'),
+              ),
+              const Text(
+                '\nDental & Vision Choices: The most compatible source for\nadding dental and vision care to your UTDallas BlueCross\nBlueShield Health Insurance Plan is with: ',
+                textAlign: TextAlign.center,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _launchDentalBrowser();
+                },
+                child: const Text('BlueCare Dental'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _launchVisionBrowser();
+                },
+                child: const Text('BlueCare Vision'),
+              ),
+              const Text(
+                '\nRenters Insurance: If living off-campus, it is important\nto secure insurance for possessions and property damage:',
+                textAlign: TextAlign.center,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _launchRenterBrowser();
+                },
+                child: const Text('StateFarm Renters Insurance'),
+              ),
+              const Text(
+                '\nTravel Insurance: When traveling back internationally, travel insurance can assist in making trips easier and headache-less:',
+                textAlign: TextAlign.center,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _launchRenterBrowser();
+                },
+                child: const Text('Seven Corners'),
+              ),
+              const Text(
+                '\n\n\n\n\n\n\n',
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Go back'),
+              ),
+            ]
         ),
       ),
     );
@@ -102,34 +217,124 @@ class CourseHelper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Course helper"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget> [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MajorIdeas()),
-                );
-              },
-              child: const Text("I need major ideas"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const CourseIdeas()),
-                );
-              },
-              child: const Text("I know my major, and I need course ideas"),
-            ),
-          ]
+        appBar: AppBar(
+          title: const Text("Course helper"),
         ),
-      )
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Insurance()),
+                    );
+                  },
+                  child: const Text("I need major ideas"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Insurance()),
+                    );
+                  },
+                  child: const Text("I know my major, and I need course ideas"),
+                ),
+              ]
+          ),
+        )
     );
+  }
+}
+
+_launchGalaxyBrowser() async {
+  const url = 'https://www.utdallas.edu/galaxy/';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchCoursebookBrowser() async {
+  const url = 'http://coursebook.utdallas.edu/';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchOITBrowser() async {
+  const url = 'https://oit.utdallas.edu/';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchBursarBrowser() async {
+  const url = 'https://bursar.utdallas.edu/';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchEBrowser() async {
+  const url = 'http://elearning.utdallas.edu/';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchInsBrowser() async {
+  const url = 'https://insurance.utdallas.edu/policies/international/';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchDentalBrowser() async {
+  const url = 'https://www.bcbstx.com/employer/products/dental/bluecare-dental-ppo';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchVisionBrowser() async {
+  const url = 'https://www.bcbstx.com/shop-plans/individual-and-family/vision-insurance-plans';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchRenterBrowser() async {
+  const url = 'https://www.statefarm.com/insurance/home-and-property/renters';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchTravelBrowser() async {
+  const url = 'https://www.sevencorners.com/#start';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
